ed5f079f60e1c6296d47722ce52dd51ee56c992f
alexsumoski <alexsumoski@gmail.com>
2023-07-14 14:49:04 -0400

    Social media sign in, in progress

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -6,11 +6,11 @@ import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
 import userRegisterModal from '@/app/hooks/userRegisterModel';
 import userLoginModal from '@/app/hooks/userLoginModel';
-import { User } from '@prisma/client';
 import { signOut } from 'next-auth/react';
+import { SafeUser } from '@/app/types';
 
 interface UserMenuProps {
-    currentUser?: User | null;
+    currentUser?: SafeUser | null;
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
@@ -20,7 +20,6 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
     }, []);
-    console.log(currentUser);
     
     return (
         <div className="relative">
@@ -31,7 +30,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                 <div onClick={toggleOpen} className="p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition">
                     <AiOutlineMenu />
                     <div className="hidden md:block">
-                        <Avatar />
+                        <Avatar src={currentUser?.image} />
                     </div>
                 </div>
             </div>
